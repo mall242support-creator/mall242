93e45a7380da1c02173e4389612d7cf58c854507
wellrootafrica <wellrootafrica@gmail.com>
2026-04-20 20:09:34 +0000

    add netlify url to cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -52,6 +52,7 @@ const corsOptions = {
     const allowedOrigins = [
       process.env.FRONTEND_URL,
       process.env.FRONTEND_URL_PROD,
+      'https://mall242.netlify.app',
       'http://localhost:5173',
       'http://localhost:3000',
       'http://127.0.0.1:5173',
